560ce6ed36c832e06099a30de44c71a283d77c77
qminhminh <hqminh050503@gmail.com>
2024-07-19 11:29:18 +0700

    add logo drive and uẻ

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema(
         profile: {
             type: String,
             require: true,
-            default: "https://d326fntlu7tb1e.cloudfront.net/uploads/bdec9d7d-0544-4fc4-823d-3b898f6dbbbf-vinci_03.jpeg"
+            default: "https://res.cloudinary.com/dp2bicmif/image/upload/v1721315877/logo_hrsyqx.png"
         },
 
     }, { timestamps: true }
